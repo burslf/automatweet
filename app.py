dd4735b724167f153dbe59f9faa3619b5df1ddc0
Yoel <yoelzerbib7@gmail.com>
2022-09-22 17:17:13 +0300

    update

--- app.py
+++ app.py
@@ -3,24 +3,11 @@ from time import sleep
 from api.lyrics_api import get_two_random_lyrics
 
 from api.twitter_api import TweetAPI
+from helpers.custom_log import get_logger
 
-def main(
-    tweeapi
-):
-    random_lyrics = get_two_random_lyrics(artist='Lil Baby')
-    
-    print("TITLE: ", random_lyrics['title'])
-    
-    formatted_lyrics = '\n'.join(random_lyrics['lyrics'])
-    
-    print("LYRICS: ", formatted_lyrics)
-    
+logger = get_logger()
 
-    tweeapi.create_tweet(
-        text=formatted_lyrics
-    )
-
-if __name__ == "__main__":
+def main(event, context):
     creds = {
         'api_key': os.environ.get('api_key'),
         'api_secret': os.environ.get('api_secret'),
@@ -31,6 +18,14 @@ if __name__ == "__main__":
 
     tweeapi = TweetAPI(creds=creds)
 
-    main(
-        tweeapi=tweeapi
+    random_lyrics = get_two_random_lyrics(artist='Lil Baby')
+    
+    logger.info("TITLE: ", random_lyrics['title'])
+    
+    formatted_lyrics = '\n'.join(random_lyrics['lyrics'])
+    
+    logger.info("LYRICS: ", formatted_lyrics)
+
+    tweeapi.create_tweet(
+        text=formatted_lyrics
     )
